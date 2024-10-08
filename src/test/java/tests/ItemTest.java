59c97c4666cf5aba81c4e8448cabba3eae6a5f4a
Cherkasova Alyona <sentyao@yandex.ru>
2024-10-08 16:45:57 +0300

    added correction

--- src/test/java/tests/ItemTest.java
+++ src/test/java/tests/ItemTest.java
@@ -1,7 +1,9 @@
 package tests;
 
 import io.qameta.allure.Severity;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import pages.ItemPage;
@@ -16,17 +18,14 @@ public class ItemTest extends TestBase {
     MainPage mainPage = new MainPage();
     ItemPage itemPage = new ItemPage();
 
-    @ValueSource(strings = {
-            "163571936",
-            "175440928"
-    })
-    @ParameterizedTest(name = "после нажатия на кнопку 'В избраное' должно появится модальное окно с выбором размера")
+    @Test
+    @DisplayName("после нажатия на кнопку 'В избраное' должно появится модальное окно с выбором размера")
     @Severity(NORMAL)
-    void checkModalWindowWithSizesIfNoGoods(String articleNumber) {
+    void checkModalWindowWithSizesIfNoGoods() {
         mainPage.openPage();
         mainPage.expectLoadingContainer();
-        mainPage.requestInSearchInput(articleNumber);
-        itemPage.checkArticleNumber(articleNumber);
+        mainPage.requestInSearchInput("163571936");
+        itemPage.checkArticleNumber("163571936");
         itemPage.checkTextButton("В избранное");
         itemPage.checkHeaderSizesItemWindow("Выберите размер");
     }
