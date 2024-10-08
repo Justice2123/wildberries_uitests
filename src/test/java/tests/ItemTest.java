e60d355ff8120ae49a8dc5109800e970115e4f73
Cherkasova Alyona <sentyao@yandex.ru>
2024-10-08 16:57:35 +0300

    added correction

--- src/test/java/tests/ItemTest.java
+++ src/test/java/tests/ItemTest.java
@@ -4,8 +4,6 @@ import io.qameta.allure.Severity;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 import pages.ItemPage;
 import pages.MainPage;
 
