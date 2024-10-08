6055f4f39790bc7685bb547cf8fe5244391eba3c
Cherkasova Alyona <sentyao@yandex.ru>
2024-10-08 14:39:07 +0300

    added correction

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -20,7 +20,7 @@ public class MainPage {
             headerAfterRequestSearchInput = $(".searching-results__title"),
             loginIcon = $(".j-main-login"),
             jobButton = $(".simple-menu__link--employment"),
-            mainContainer = $(".banners-catalog-custom__container"),
+            mainContainer = $(".main-page"),
             jobPageButton = $(byTagAndText("a", "Работа в Wildberries"));
     private final ElementsCollection
             navMenuItems = $$x("//div[@id='basketContent']/div/a");
